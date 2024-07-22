16076afaa5802cc581c37b032066a60bcaf969da
Peirong Liu <44064021+peirong26@users.noreply.github.com>
2024-07-22 09:03:23 -0400

    added notes in pre-processing: images need to be resampled to 1x1x1 first

--- preprocess/generation_labels.py
+++ preprocess/generation_labels.py
@@ -2,7 +2,7 @@
 Data preprocessing for PEPSI training
 
 Steps
-1) SynthSR: map to a T1 contrast for inpainting the pathology and better Skull-stripping/SynthSeg performance;
+1) SynthSR: map to a T1 contrast for pseudo-inpainting the pathology and better Skull-stripping/SynthSeg performance;
 2) SynthStrip: skull-stripping; saving the stripped T1 as the supervision ground truth;
 3) SynthSeg: get segmentation labels;
 4) Merge left & right labels for obtaining the anatomy generation labels.
@@ -22,6 +22,9 @@ os.system('source $FREESURFER_HOME/SetUpFreeSurfer.sh')
 # your downloaded images
 root_dir = '/path/to/your/dataset'
 img_dir = os.path.join(root_dir, 'T1_orig') 
+# NOTE: original images need to be of 1x1x1 resolution
+# if not, you need to reample them first, e.g., SynthSeg can do the resampling as following:
+# os.system('mri_synthseg' + ' --i ' + '/path/to/original/image' + ' --o ' + '/path/to/temporary/synthseg/image' + ' --resample ' + '/path/to/save/the/resampled/image')
 
 # pre-processing
 img_strip_save_dir = utils.make_dir(os.path.join(root_dir, 'T1')) 
@@ -40,27 +43,28 @@ subjs.sort()
 
 for i, name in enumerate(subjs): 
     if name.startswith('sub-') and name.endswith('.nii'):
+        basename = name.split('.')[0]
         print('Now processing: %s (%d/%d)' % (name, i+1, len(subjs)))
 
-        if not os.path.isfile(os.path.join(synthsr_save_dir, name + '.nii')):
+        if not os.path.isfile(os.path.join(synthsr_save_dir, basename + '.nii')):
             print('  synthsr-ing')
-            os.system('mri_synthsr' + ' --i ' + os.path.join(img_dir, name + '.nii') + ' --o ' + os.path.join(synthsr_save_dir, name + '.nii'))
+            os.system('mri_synthsr' + ' --i ' + os.path.join(img_dir, basename + '.nii') + ' --o ' + os.path.join(synthsr_save_dir, basename + '.nii'))
 
-        if not os.path.isfile(os.path.join(img_strip_save_dir, name + '.nii')):
+        if not os.path.isfile(os.path.join(img_strip_save_dir, basename + '.nii')):
             print('  synthstrip-ing')
-            os.system('mri_synthstrip' + ' -i ' + os.path.join(synthsr_save_dir, name + '.nii') + ' -o ' + os.path.join(synthstrip_save_dir, name + '.nii') + ' -m ' + os.path.join(mask_save_dir, name + '.nii'))
-            mask, aff = utils.MRIread(os.path.join(mask_save_dir, name + '.nii'))
-            img, aff = utils.MRIread(os.path.join(img_dir, name + '.nii'))
+            os.system('mri_synthstrip' + ' -i ' + os.path.join(synthsr_save_dir, basename + '.nii') + ' -o ' + os.path.join(synthstrip_save_dir, basename + '.nii') + ' -m ' + os.path.join(mask_save_dir, basename + '.nii'))
+            mask, aff = utils.MRIread(os.path.join(mask_save_dir, basename + '.nii'))
+            img, aff = utils.MRIread(os.path.join(img_dir, basename + '.nii'))
             img *= mask # skull-stripping
-            utils.MRIwrite(img, aff, os.path.join(img_strip_save_dir, name + '.nii'))
+            utils.MRIwrite(img, aff, os.path.join(img_strip_save_dir, basename + '.nii'))
 
-        if not os.path.isfile(os.path.join(synthseg_save_dir, name + '.nii')):
+        if not os.path.isfile(os.path.join(synthseg_save_dir, basename + '.nii')):
             print('  synthseg-ing')
-            os.system('mri_synthseg' + ' --i ' + os.path.join(synthsr_save_dir, name + '.nii') + ' --o ' + os.path.join(synthseg_save_dir, name + '.nii'))
-        
-        if not os.path.isfile(os.path.join(gen_save_dir, name + '.nii')):
+            os.system('mri_synthseg' + ' --i ' + os.path.join(synthsr_save_dir, basename + '.nii') + ' --o ' + os.path.join(synthseg_save_dir, basename + '.nii'))
+
+        if not os.path.isfile(os.path.join(gen_save_dir, basename + '.nii')):
             print('  genseg-ing')
-            im, aff = utils.MRIread(os.path.join(synthseg_save_dir, name + '.nii'), im_only=False, dtype='float')
+            im, aff = utils.MRIread(os.path.join(synthseg_save_dir, basename + '.nii'), im_only=False, dtype='float')
             for l in utils.right_to_left_dict.keys():
                 im[im==l] = utils.right_to_left_dict[l]
-            utils.MRIwrite(im, aff, os.path.join(gen_save_dir, name + '.nii'))
+            utils.MRIwrite(im, aff, os.path.join(gen_save_dir, basename + '.nii'))
